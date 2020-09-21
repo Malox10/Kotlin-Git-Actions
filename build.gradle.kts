ee15c2d68cd04fe0df0b6cfab83fb4c7affda6a9
MaxiJ <Maxi.Junges@gmx.de>
2020-09-22 00:13:47 +0200

    logging for tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     kotlin("jvm") version "1.4.10"
 }
@@ -13,4 +16,49 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("junit:junit:4.12")
+}
+
+tasks.withType<Test> {
+    testLogging {
+        // set options for log level LIFECYCLE
+        events = setOf(
+                TestLogEvent.FAILED,
+                TestLogEvent.PASSED,
+                TestLogEvent.SKIPPED,
+                TestLogEvent.STANDARD_OUT
+        )
+        exceptionFormat = TestExceptionFormat.FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+
+        // set options for log level DEBUG and INFO
+        debug {
+            events = setOf(
+                    TestLogEvent.STARTED,
+                    TestLogEvent.FAILED,
+                    TestLogEvent.PASSED,
+                    TestLogEvent.SKIPPED,
+                    TestLogEvent.STANDARD_ERROR,
+                    TestLogEvent.STANDARD_OUT
+            )
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+        info.events = debug.events
+        info.exceptionFormat = debug.exceptionFormat
+
+        afterSuite(KotlinClosure2<TestDescriptor, TestResult, Unit>({ desc, result ->
+            if (desc.parent != null) { // will match the outermost suite
+                val output = "Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} passed, ${result.failedTestCount} failed, ${result.skippedTestCount} skipped)"
+                val startItem = "|  "
+                val endItem = "  |"
+                val repeatLength = startItem.length + output.length + endItem.length
+                println(
+                        "\n" + ("-".repeat(repeatLength)) +
+                        "\n" + startItem + output + endItem +
+                        "\n" + ("-".repeat(repeatLength))
+                )
+            }
+        }))
+    }
 }
